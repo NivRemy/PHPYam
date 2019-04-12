515c5c673e86a9440c9f07adac4aaf3b218bea8b
Rémy NIVOIS <remynivois@adrar-formation.com>
2019-04-12 09:18:18 +0200

    added some styling

--- index.php
+++ index.php
@@ -3,53 +3,68 @@
 <head>
     <meta charset="UTF-8">
     <title>Jeu de dé</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="yam.css">
 </head>
 <body>
+	<div class="container">
+		<?php
+		if (isset($_GET['playerNumber'])){
+			?>
+		<form method="post" action="yam.php">
+			<?php
+			for($i=1;$i<=$_GET['playerNumber'];$i++){
+			?>
+			<div>
+				<label>Joueur <?= $i; ?></label>
+				<input type="text" name="player<?= $i; ?>" required>
+			</div>
+			<?php
+			}
+			?>
+			<button type="submit" name="pNumber" value="<?= $_GET['playerNumber']; ?>">Jouer</button>
+		</form>
+		
 	<?php
-	if (isset($_GET['playerNumber'])){
-		?>
-	<form method="post" action="yam.php">
-		<?php
-		for($i=1;$i<=$_GET['playerNumber'];$i++){
-		?>
-		<div>
-			<label>Joueur <?= $i; ?></label>
-			<input type="text" name="player<?= $i; ?>" required>
-		</div>
-		<?php
-		}
-		?>
-		<button type="submit" name="pNumber" value="<?= $_GET['playerNumber']; ?>">Jouer</button>
-	</form>
-	
-<?php
 
-} else {
+	} else {
 
+		?>
+		<h1>Yam's</h1>
+		<form method="post" action="yam.php">
+			<div class="form-group">
+				<label for="P1">Joueur 1</label>
+				<input type="text" name="player1" id="P1" required>
+			</div>
+			<div class="form-group">
+			<label for="P2">Joueur 2</label>
+				<input type="text" id="P2" name="player2" required>
+			</div>
+			<div class="form-group">
+				<button type="submit" name="pNumber" value="2" class="newGame btn btn-primary">Nouvelle Partie
+				</button>
+			</div>
+		</form>
+
+		<h2>Plus de Joueurs?</h2>
+		<form method="get">
+			<div class="form-group">
+			<label for="NbP">Choisissez le nombre de joueurs</label>
+				<select id="NbP" name="playerNumber">
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<button class="newGame btn btn-primary" type="submit">Jouer
+				</button>
+			</div>
+		</form>
+	<?php	
+	}
 	?>
-	<form method="post" action="yam.php">
-		<label>Joueur 1</label>
-		<input type="text" name="player1" required>
-		<label>Joueur 2</label>
-		<input type="text" name="player2" required>
-		<button type="submit" name="pNumber" value="2" class="newGame">Nouvelle Partie</button>
-	</form>
-
-	<h2>Plus de Joueurs?</h2>
-	<form method="get">
-		<label>Choisissez le nombre de joueurs</label>
-		<select name="playerNumber">
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-		</select>
-		<button class="newGame" type="submit">Jouer</button>
-	</form>
-<?php
-}
-?>
-
+	</div>
 </body>
 </html>
--- yam.php
+++ yam.php
@@ -7,26 +7,48 @@ include_once 'player.php';
 <head>
     <meta charset="UTF-8">
     <title>Jeu de dé</title>
-    <link rel="stylesheet" type="text/css" href="yam.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="yam.css">
 </head>
 <body>
-	<form method="post" action="index.php">
-		<label>Joueur 1</label>
-		<input type="text" name="player1" required>
-		<label>Joueur 2</label>
-		<input type="text" name="player2" required>
-		<button type="submit" name="pNumber" value="2" class="newGame">Nouvelle Partie</button>
-	</form>
-	<form method="get" action="index.php">
-		<label>Choisissez le nombre de joueurs</label>
-		<select name="playerNumber">
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-		</select>
-		<button class="newGame" type="submit">Jouer</button>
-	</form>
+	<div class="container playmenu">
+		<div class="play-menu"><h1>Yam's</h1></div>
+		<div class="play-menu"><div class="dropdown">	
+  			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	    	Nouvelle Partie
+  			</button>
+  			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<form method="post" action="index.php">
+					<div class="form-group">	
+						<label>Joueur 1</label>
+						<input type="text" name="player1" required>
+					</div>
+					<div class="form-group">
+						<label>Joueur 2</label>
+						<input type="text" name="player2" required>
+					</div>
+					<div class="form-group">
+						<button type="submit" name="pNumber" value="2" class="newGame">Partie 2 Joueurs
+						</button>
+					</div>
+				</form>
+				<form method="get" action="index.php">
+					<div class="form-group">
+						<label>Choisissez le nombre de joueurs</label>
+						<select name="playerNumber">
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<button class="newGame" type="submit">Nouvelle partie</button>
+					</div>
+				</form>
+			</div></div>
+		</div>
+	</div>
 </br></br>
 <?php
 if (isset($_POST['pNumber'])){
@@ -78,6 +100,9 @@ displayScoreTables($game,$endOfGame);
 $_SESSION['yamGame'] = serialize($game);
 
 ?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
 
@@ -90,7 +115,7 @@ function displayDices($game){
 	$rerollsLeft = $game->getCurrentPlayerRerolls();
 			
 	foreach ($bucketValues as $key => $value) {
-		echo '<div class="reroll"><label>Le dé ' . $key . ' a fait ' . $value . ', relancer : </label>';
+		echo '<div class="reroll"><label>Le dé ' . ($key+1) . ' a fait ' . $value . ', relancer : </label>';
 		if ($rerollsLeft >0) {
 		echo '<input type="checkbox" name="toReroll[]" value="' . $key . '"/>';
 		}
